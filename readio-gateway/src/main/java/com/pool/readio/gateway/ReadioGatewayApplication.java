b7b6dbbe07105591955754056cfe8b177daef3a4
Yang Zi <2959243019@qq.com>
2026-02-23 02:28:33 +0800

    fix: 为readio-gateway开启 DiscoveryClient

--- readio-gateway/src/main/java/com/pool/readio/gateway/ReadioGatewayApplication.java
+++ readio-gateway/src/main/java/com/pool/readio/gateway/ReadioGatewayApplication.java
@@ -1,8 +1,9 @@
 package com.pool.readio.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ReadioGatewayApplication {
 
